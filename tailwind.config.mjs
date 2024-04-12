53657aa7d44ee721bc9db8dbcdf1a1c67bc4f0b6
ArmandoCode <totovip1464@gmail.com>
2024-04-12 09:05:28 -0600

    add ligth mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'selector',
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				custom: '-1px 0px 66px 45px #0A0E1D',
+				customWhite: '-1px 0px 66px 45px #ffffff',
+			},
+			backgroundImage: {
+				'fade': 'linear-gradient(90deg, #0A0E1D 0%, transparent 30%, transparent 70%, #0A0E1D 100%)',
+				'fade-white': 'linear-gradient(90deg, #ffffff 0%, transparent 30%, transparent 70%, #ffffff 100%)',
+			},
+		},
 	},
 	plugins: [
 		require('@headlessui/tailwindcss'),
