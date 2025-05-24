3aae69bc8195b34c58fde525b7b741174b422304
armandocodecr <totovip1464@gmail.com>
2025-05-24 11:00:02 -0600

    feat: add another experience

--- src/constants/Experience.ts
+++ src/constants/Experience.ts
@@ -1,5 +1,19 @@
 export const EXPERIENCE = [
     {
+      title: "Trustless Work",
+      description:
+        `- Desarrollo contratos inteligentes robustos y seguros que cumplan con el modelo de negocio establecido para la empresa. Mi enfoque en la seguridad y la eficiencia garantiza que los contratos sean confiables y cumplan con los estándares más altos de la industria.
+
+- Gestiono API's que aseguran una correcta y segura conexión de esta misma con el contrato inteligente desplegado. Esto incluye la implementación de protocolos de seguridad avanzados y la optimización del rendimiento para garantizar una experiencia fluida y segura para los usuarios.
+
+- Lidero el desarrollo de productos y estrategias tecnológicas en Trustless Work, una plataforma Web3 que facilita pagos seguros con escrows inteligentes. Me enfoco en diseñar soluciones escalables, colaborar con el equipo en decisiones clave y fomentar la innovación en el ecosistema blockchain.
+
+Learn more about Trustless Work here: <a class="text-blue-500 hover:cursor-pointer" href="https://trustlesswork.com/" target="_blank" rel="noopener noreferrer">Website</a>
+`,
+      date: "Junio 2024 - Actualidad",
+      stack: ["Rust", "Blockchain", "NestJS", "Stellar", "Firebase"],
+    },
+    {
       title: "Morphosis Blocks",
       description:
         `- Especializo en el desarrollo web, enfocándome en la experiencia de usuario y la identidad de marca para establecer una presencia online destacada. Mis proyectos de automatización de procesos, especialmente en contabilidad, han optimizado significativamente el uso de recursos en la empresa.
